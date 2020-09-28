03eaa58e92be2562803d01127f9af26b9babbe39
DannyvdZwart <32216@ma-web.nl>
2020-09-28 14:37:15 +0200

    first commit

---
+++ ditbenik.py
@@ -0,0 +1,143 @@
+print("Hello You!, ik ben Danny")
+print("Wie ben jij?")
+naam = input()
+print("Hello " +  naam)
+print = input
+print("Ik ben een nieuwkomer op het Mediacollege Amsterdam.") 
+print("Door een aantal vragen over mij te beantwoorden leer je mij beter kennen. ")
+print("")
+
+input("Voor dat ik naar het Media College Amsterdam kwam, zat ik op de school? \n A.Wellantcollege Westplas Mavo \n B.Hermann Wesselink College \n C.Amstelveen College.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "A" or antwoord == "a" or antwoord == "Wellantcollege Westplas Mavo":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: Het Wellantcollege Westplas Mavo.")
+    print("")
+
+print("")
+input("Wat is mijn volledige naam? \n A.Danny Rogier van der Zwart \n B.Danny Ramon van der Zwart \n C.Danny Reffelino van der Zwart.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "B" or antwoord == "b" or antwoord == "Danny Ramon van der Zwart":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: Danny Ramon van der Zwart.")
+    print("")
+
+print("")
+
+input("Wat is zijn lievelings kleuren? \n A.Groen en Geel \n B.Rood en Paars \n C.Groen en Zwart.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "C" or antwoord == "c" or antwoord == "Groen en Zwart":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: Groen en Zwart.")
+    print("")
+
+print("")
+
+input("Op welke sport heb ik gezeten en doe ik nu? \n A.Voetbal en Hockey \n B.Rugby en Hockey \n C.Rugby en Voetbal.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "C" or antwoord == "c" or antwoord == "Rugby en Voetbal":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: Rugby en Voetbal.")
+    print("")
+
+print("")
+
+input("Wat zijn mijn hobbies? \n A.Gamen, Schaken en Lezen \n B.Gamen, Sporten en Sport kijken \n C.Gamen, Sporten en Koken.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "B" or antwoord == "b" or antwoord == "Gamen, Sporten en Sport kijken":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: Gamen, Sporten en Sport kijken.")
+    print("")
+
+print("")
+
+input("Hoe oud ben ik? \n A.16 \n B.17 \n C.18.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "B" or antwoord == "b" or antwoord == "17":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: 17.")
+    print("")
+
+print("")
+
+input("Wat zijn mijn favorite games? \n A.Rocket league en Minecraft \n B.Gta V en Bo4 \n C.CSGO en Overwatch.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "A" or antwoord == "a" or antwoord == "Rocket league en Minecraft":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: Rocket league en Minecraft.")
+    print("")
+
+print("")
+
+input("Wat is de hoogste rank die ik gehaald heb in Rocket league? \n A.Grand Champion \n B.Platinum 2 \n C.Diamond 3.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "A" or antwoord == "a" or antwoord == "Grand Champion":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: Grand Champion.")
+    print("")
+
+print("")
+
+input("Waar woon ik? \n A.Amsterdam \n B.Amstelveen \n C.Aalsmeer.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "C" or antwoord == "c" or antwoord == "Aalsmeer":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: Aalsmeer.")
+    print("")
+
+print("")
+
+input("Hoeveel broers en zussen heb ik? \n A.1 broer en 1 zusje \n B.2 broertjes \n C.1 zusje.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "C" or antwoord == "c" or antwoord == "1 zusje":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: 1 zusje.")
+    print("")
+
+print("")
+
+input("Werk ik? \n A.ja \n B.nee \n C.sws enge gammasessies.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "A" or antwoord == "a" or antwoord == "C" or antwoord == "c" or antwoord == "ja" or antwoord == "sws enge gammasessies":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: A of sws enge gammasessies.")
+    print("")
+
+print("")
+
+input("Wat is mijn favorite eten? \n A.Patat \n B.Lasagne \n C.Bami.")
+antwoord = input("Typ hier uw antwoord:  ")
+if antwoord == "B" or antwoord == "b" or antwoord == "Lasagne":
+    print("Het anwoord is juist!")
+    print("")
+else:
+    print("Het antwoord is onjuist. Het goede antwoord: Lasagne.")
+    print("")
+
+print("")
+
+print("Dat waren de 12 vragen die ik voor je had") 
+print("Ik hoop dat je me nu een beetje beter hebt leren kennen") 
+print("Ik vond het heel leuk ik hoop jij ook en nog een fijne dag " +  naam)
